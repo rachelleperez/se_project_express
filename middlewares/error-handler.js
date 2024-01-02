66afc04d650e9d25c5baaeec43580b7bc40b734e
Rachelle Perez <rachelleaperez@gmail.com>
2024-01-02 01:01:30 -0500

    refactor attempt 2

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -5,36 +5,28 @@ const {
   InternalServerError,
 } = require("../utils/errors/index");
 
-module.exports = function errorHandler(errIn, req, res, next) {
-  console.log("Error received by errorHandler", errIn);
-  // console.log(errIn instanceof Error);
-  // console.log(errIn.constructor !== Error);
+module.exports = function errorHandler(err, req, res, next) {
+  console.log("Error received by errorHandler", err);
 
-  // variable to choose appropriate error to use
-  let err = null;
+  // // if standard error (not instances of error), use appropriate custom Error object
+  // if (err.name === "ValidationError") {
+  //   err = new BadRequestError(ERROR_MSG.validation);
+  // } else if ((!err) instanceof Error && err.name === "DocumentNotFoundError") {
+  //   err = new NotFoundError(ERROR_MSG.unknownId);
+  // } else if ((!err) instanceof Error && err.name === "CastError") {
+  //   err = new BadRequestError(ERROR_MSG.invalidID);
+  // }
 
-  // if standard error (not instances of error), use appropriate custom Error object
-  if ((!errIn) instanceof Error && errIn.name === "ValidationError") {
-    err = new BadRequestError(ERROR_MSG.validation);
-  } else if (
-    (!errIn) instanceof Error &&
-    errIn.name === "DocumentNotFoundError"
-  ) {
-    err = new NotFoundError(ERROR_MSG.unknownId);
-  } else if ((!errIn) instanceof Error && errIn.name === "CastError") {
-    err = new BadRequestError(ERROR_MSG.invalidID);
+  if (err instanceof Error) {
+    console.error(err.message);
+    return res
+      .status(err.status)
+      .send({ name: err.name, message: err.message });
+  } else {
+    newErr = new InternalServerError(ERROR_MSG.unexpectedError);
+    console.error(newErr.message);
+    return res
+      .status(newErr.status)
+      .send({ name: newErr.name, message: newErr.message });
   }
-  // custom Error objects set in utils.error (object is an Error but not specifically "Error")
-  else if (errIn instanceof Error && errIn.constructor !== Error) {
-    err = errIn;
-  }
-  // default
-  else {
-    err = new InternalServerError(ERROR_MSG.internalServerError);
-  }
-
-  // console.error all errors
-  console.error(err.message);
-
-  return res.status(err.status).send({ name: err.name, message: err.message });
 };
