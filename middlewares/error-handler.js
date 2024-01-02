e3e143f6238922b51676086915c54f0cde7ebabe
Rachelle Perez <rachelleaperez@gmail.com>
2024-01-02 00:55:51 -0500

    bug in errorHandler, checking err instead of errIn

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -7,20 +7,25 @@ const {
 
 module.exports = function errorHandler(errIn, req, res, next) {
   console.log("Error received by errorHandler", errIn);
+  // console.log(errIn instanceof Error);
+  // console.log(errIn.constructor !== Error);
 
   // variable to choose appropriate error to use
   let err = null;
 
   // if standard error (not instances of error), use appropriate custom Error object
-  if ((!err) instanceof Error && err.name === "ValidationError") {
+  if ((!errIn) instanceof Error && errIn.name === "ValidationError") {
     err = new BadRequestError(ERROR_MSG.validation);
-  } else if ((!err) instanceof Error && err.name === "DocumentNotFoundError") {
+  } else if (
+    (!errIn) instanceof Error &&
+    errIn.name === "DocumentNotFoundError"
+  ) {
     err = new NotFoundError(ERROR_MSG.unknownId);
-  } else if ((!err) instanceof Error && err.name === "CastError") {
+  } else if ((!errIn) instanceof Error && errIn.name === "CastError") {
     err = new BadRequestError(ERROR_MSG.invalidID);
   }
   // custom Error objects set in utils.error (object is an Error but not specifically "Error")
-  else if (err instanceof Error && !err.constructor === Error) {
+  else if (errIn instanceof Error && errIn.constructor !== Error) {
     err = errIn;
   }
   // default
@@ -31,7 +36,5 @@ module.exports = function errorHandler(errIn, req, res, next) {
   // console.error all errors
   console.error(err.message);
 
-  return res
-    .status(err.status || 500)
-    .send({ name: err.name, message: err.message });
+  return res.status(err.status).send({ name: err.name, message: err.message });
 };
