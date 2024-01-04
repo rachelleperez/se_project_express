0e28d729df6f2cbaee6fce51dbff102b7caaaa22
Rachelle Perez <rachelleaperez@gmail.com>
2024-01-03 23:16:38 -0500

    fixed bug related to or in validateId 3

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 
 const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
-// const { validateId } = require("../middlewares/validation");
+const { validateId } = require("../middlewares/validation");
 
 // CRUD
 
 // Create
 
 // Read
-router.get("/me", getCurrentUser);
+router.get("/me", validateId, getCurrentUser);
 
 // Update
-router.patch("/me", updateCurrentUser);
+router.patch("/me", validateId, updateCurrentUser);
 
 module.exports = router;
